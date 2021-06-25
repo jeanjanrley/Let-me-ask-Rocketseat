501ffe99884a58081c4b92a6ad0cc60a6f884978
Jean Janrley <jeanjanrley@outlook.com>
2021-06-25 13:41:05 -0300

    funcionalidade de likes funcionando 100%

---
+++ .history/src/hooks/useRoom_20210625132134.ts
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react"
+import { database } from "../services/firebase";
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    IsAnswered: boolean;
+    IsHighlighted: boolean;
+    likes: Record<string, {
+        authorId: string;
+    }>;
+
+}>
+
+type QuestionType = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    IsAnswered: boolean;
+    IsHighlighted: boolean;
+}
+
+
+
+export function useRoom(roomId: string){
+    const {} useAuth
+    const [questions, setQuestions] = useState<QuestionType[]>([])
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`)
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FirebaseQuestions =  databaseRoom.questions ?? {}
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return{
+                    id: key,
+                    author: value.author,
+                    content: value.content,
+                    IsAnswered: value.IsAnswered,
+                    IsHighlighted: value.IsHighlighted,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    hasLiked: 
+                }
+            })
+            
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestions)
+        })
+    }, [roomId])
+
+    return {questions, title}
+}
---
+++ .history/src/hooks/useRoom_20210625132203.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react"
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    IsAnswered: boolean;
+    IsHighlighted: boolean;
+    likes: Record<string, {
+        authorId: string;
+    }>;
+
+}>
+
+type QuestionType = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    IsAnswered: boolean;
+    IsHighlighted: boolean;
+}
+
+
+
+export function useRoom(roomId: string){
+    const {user} useAuth();
+    const [questions, setQuestions] = useState<QuestionType[]>([])
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`)
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FirebaseQuestions =  databaseRoom.questions ?? {}
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return{
+                    id: key,
+                    author: value.author,
+                    content: value.content,
+                    IsAnswered: value.IsAnswered,
+                    IsHighlighted: value.IsHighlighted,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    hasLiked: Object.keys(value.likes)
+                }
+            })
+            
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestions)
+        })
+    }, [roomId])
+
+    return {questions, title}
+}
